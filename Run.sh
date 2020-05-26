316e260fe107e011c8c053a3cd5d7c81dc3f58b2
Hugo <hugoLeroy@yahoo.fr>
2020-05-26 11:12:09 +0200

    the queue type is now in run.sh

--- Run.sh
+++ Run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 SerieNum=0
+QueueType=qbigmem
 
 rm -rf "Res/Serie$SerieNum"
 mkdir "Res/Serie$SerieNum"
 
 cp Parameter.py "Res/Serie$SerieNum/Parameter.py"
 
-sbatch MonoAggregateAnnealing.pbs $SerieNum
+sbatch MonoAggregateAnnealing.pbs $SerieNum $QueueType
